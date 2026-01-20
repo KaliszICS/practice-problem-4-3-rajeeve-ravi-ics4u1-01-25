969aeccbbb76946d8ff24d730889b86f089887c4
rajeeve-ravi-ics4u1-01-25 <rajeeve.ravi@student.tdsb.on.ca>
2026-01-20 13:05:49 -0500

    Update PracticeProblem.java
    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,131 @@
+/**
+ * The type Practice problem.
+ */
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+    /**
+     * Main.
+     *
+     * @param args the args
+     */
+    public static void main(String args[]) {
+        // Main method for testing
+    }
 
-	}
+}
 
-	public static void q1() {
-		//Write question 1 code here
-	}
+/**
+ * The type Rectangle.
+ */
+class Rectangle {
+    private double length;
+    private double width;
 
-	public static void q2() {
-		//Write question 2 code here
-	}
+    /**
+     * Instantiates a new Rectangle.
+     */
+// No-arg constructor: sets length to 4 and width to 8
+    public Rectangle() {
+        this.length = 4.0;
+        this.width = 8.0;
+    }
 
-	public static void q3() {
-		//Write question 3 code here
-	}
+    /**
+     * Instantiates a new Rectangle.
+     *
+     * @param length the length
+     */
+// Single-arg constructor: sets length to provided value and width to 8
+    public Rectangle(double length) {
+        this.length = length;
+        this.width = 8.0;
+    }
 
-	public static void q4() {
-		//Write question 4 code here
-	}
+    /**
+     * Instantiates a new Rectangle.
+     *
+     * @param length the length
+     * @param width  the width
+     */
+// Two-arg constructor: sets both length and width
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
 
-	public static void q5() {
-		//Write question 5 code here
-	}
+    /**
+     * Gets length.
+     *
+     * @return the length
+     */
+    public double getLength() {
+        return length;
+    }
 
+    /**
+     * Gets width.
+     *
+     * @return the width
+     */
+    public double getWidth() {
+        return width;
+    }
+}
+
+/**
+ * The type Chair.
+ */
+class Chair {
+    private int legs;
+    private String material;
+
+    /**
+     * Instantiates a new Chair.
+     */
+// No-arg constructor: sets legs to 4 and material to "wood"
+    public Chair() {
+        this.legs = 4;
+        this.material = "wood";
+    }
+
+    /**
+     * Instantiates a new Chair.
+     *
+     * @param legs the legs
+     */
+// Single-arg constructor: sets legs to provided value and material to "wood"
+    public Chair(int legs) {
+        this.legs = legs;
+        this.material = "wood";
+    }
+
+    /**
+     * Instantiates a new Chair.
+     *
+     * @param legs     the legs
+     * @param material the material
+     */
+// Two-arg constructor: sets both legs and material
+    public Chair(int legs, String material) {
+        this.legs = legs;
+        this.material = material;
+    }
+
+    /**
+     * Gets legs.
+     *
+     * @return the legs
+     */
+    public int getLegs() {
+        return legs;
+    }
+
+    /**
+     * Gets material.
+     *
+     * @return the material
+     */
+    public String getMaterial() {
+        return material;
+    }
 }
